2dd5ebcb005bf5c1c9aa1149ee7c4ce60d815bc6
Kaiden Zapanta <kaidenzapanta@gmail.com>
2024-09-05 18:53:44 -0500

    added game reset

--- GameManager.cs
+++ GameManager.cs
@@ -6,16 +6,10 @@ public partial class GameManager : Node
 {
     public static GameManager Instance { get; private set; }
 	private int Score = 0;
-	private int NumDrops = 0;
     public override void _Ready()
     {
         Instance = this;
     }
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
 
 	public void OnGameLost() 
 	{
@@ -25,5 +19,9 @@ public partial class GameManager : Node
 	{
 		Score += sc;
 		GD.Print(Score);
+	}
+	public void StartNewGame() {
+		Score = 0;
+		GetTree().ReloadCurrentScene();
 	}
 }
